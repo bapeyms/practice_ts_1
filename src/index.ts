4b2f9b1cbbc4266fb83ec5f5394b6d36e11b5c88
katherine <katerynaunik@gmail.com>
2026-09-07 12:52:41 +0300

    homework

--- src/index.ts
+++ src/index.ts
@@ -49,18 +49,29 @@ class Player {
     }
 
     attack() :void {
-        console.log();
+        this.weapon.fight();
     }
 
     print() :void {
         console.log(`Nickname: ${this.nickname}`);
         console.log(`HP: ${this.hp}`);
-        console.log(`Score: ${this.score}`);
-        console.log(`Weapon: ${this.weapon}`);
+        console.log(`Score: ${this.score}\n`);
     }
 }
 
 let weapon1: Weapon = new Axe("Double-bit axe");
+let weapon2: Weapon = new AtomicBomb("Atomic bomb");
+let weapon3: Weapon = new Ballista("Heavy balista");
+
 let player1: Player = new Player("pro228", 6767, 666, weapon1);
+let player2: Player = new Player("pro228", 2288, 333, weapon2);
+let player3: Player = new Player("pro228", 4242, 123, weapon3);
+
 
 player1.print();
+player2.print();
+player3.print();
+
+player1.attack();
+player2.attack();
+player3.attack();
